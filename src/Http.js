e717de5ef6790fe507df7bbe12a0b80a1e8b05a1
Muhammad Umer <umerqaiser1009@gmail.com>
2023-01-07 04:12:51 +0500

    Setup the home page

---
+++ src/Http.js
@@ -0,0 +1,10 @@
+import { Axios } from "axios";
+
+export default Axios.create({
+    // localhost 8000 isliye diya hai takay laravel sai connect kre kyunke laravel 8000 port pr chlte hai 
+    // axios ki madad sai react ko connect kr rahay hai laravel sai
+    baseURL:"http://localhost:8000/api",
+    headers:{
+        "Content-type": "application/json"
+    }
+})
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import http from "../Http";
 
 export default function Home() {
+    const [users, setUsers] = useState({});
+
+    useEffect(()=>{
+        fetchAllUsers();
+    }, []);
+
+    const fetchAllUsers = () => {
+        http.get('/users').then(res => {
+            setUsers(res.data);
+        })
+    }
+
   return (
-    <div>Home</div>
+    <div>
+        <h2>Users Listing</h2>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">S.No</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.matp((user, index) => (
+                    <tr>
+                        <th scope="row" key={user.id}>{index}</th>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>@mdo</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
   )
 }
